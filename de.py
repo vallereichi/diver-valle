4905fcea21c8c765148f93651f0ad74da250a48a
Valentin Reichenspurner <vreichenspurner@tum.de>
2024-11-13 16:02:29 +0100

    implemented mutation

--- de.py
+++ de.py
@@ -21,7 +21,7 @@ def create_base_vector(D:int, ranges:np.array) -> np.array:
     return np.array(X_i)
 
 
-def initialize_generation(D:int, ranges:np.array, NP:int = None) -> np.array:
+def initialize_generation(D:int, ranges:np.array, NP:int = None) -> list[np.array]:
     """
     Function to initialize the first generation of base vectors used for the differential evolution.
     Per default it creates each point by random sampling each dimension from from the specified range.
@@ -49,13 +49,47 @@ def initialize_generation(D:int, ranges:np.array, NP:int = None) -> np.array:
         np.append(X, create_base_vector(D, ranges))
         
 
-    print(f"[INFO]: initialized first genration with {NP} unique base vectors of dimension {D}")
-    return X
+    print(f"[INFO]: initialized first generation with {NP} unique base vectors of dimension {D}")
+    return list(X)
 
 
 
     
 # Step2: Mutation
+def mutation(current_population:np.array, F:float) -> np.array:
+    """
+    performs the mutation step of the differential evolution by adding a scaled difference vector
+    to a base base vector.
+
+    parameters:
+        current_population: array of points with the currrent best likelihood
+        F: scaling factor for the difference vector (range: 0 <= F <= 1)
+
+    reuturns:
+        array of size NP containing the donor vectors
+    """
+    if F <= 0 or F >= 1:
+        raise "[ERROR]: scaling factor F is out of range"
+    
+    donor_vectors = []
+
+    for i in range(len(current_population)):
+        base_vectors = random.sample(current_population, 3)
+
+        while np.array_equal(current_population[i], base_vectors[0]) or \
+              np.array_equal(current_population[i], base_vectors[1]) or \
+              np.array_equal(current_population[i], base_vectors[2]) :
+            base_vectors = random.sample(current_population, 3)
+
+        X_1, X_2, X_3 = base_vectors
+        donor_vectors.append(X_1 + F * (X_2 - X_3))
+    
+    return np.array(donor_vectors)
+        
+
+
+
+
 # Step3: Crossover
 # Step4: Selection
 # Step5: Break conditions
